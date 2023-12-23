ad512e9d1c08a20bdc2125bfe912abb75e2f50f6
JD2198 <110241674+JD2198@users.noreply.github.com>
2023-12-23 07:16:15 +0000

    var

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const id = 219
+let name= "Ram"
+var age = 24         
+place = "Asia"
+let state
+
+// id = 34
+name ="Syamm"
+age = 9
+place ="Denmark"
+console.log(id);
+console.table([id,name,age,place,state]);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
