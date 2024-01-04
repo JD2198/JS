07edf1037c34bdb2d95d9eec206c44c08ad92eee
JD2198 <110241674+JD2198@users.noreply.github.com>
2024-01-04 09:49:11 +0000

    for each

--- 05_iteration/05_for_each.js
+++ 05_iteration/05_for_each.js
@@ -3,6 +3,31 @@
 const coding = ["js","ruby","java","python"]
 
 coding.forEach( function (val){
-    console.log(val);
+    // console.log(val);
 })
 
+// to get value of an object which is inside an array
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+]
+
+// myCoding.forEach( (item) => {
+    
+//     console.log(item.languageName);
+// } )
+
+myCoding.forEach( function (item){
+    console.log(item.languageName);
+})
