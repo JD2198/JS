117a855c6ecfd24eb466a74e845a53e7c32a1f58
JD2198 <110241674+JD2198@users.noreply.github.com>
2024-01-04 06:47:42 +0000

    while_dowhile

---
+++ 05_iteration/02_while.js
@@ -0,0 +1,21 @@
+// while
+let i=0
+while(i<=10){
+    console.log(i);
+    i++
+}
+
+let myArray = ['red','yellow','green']
+let k=0
+while(k<myArray.length){
+    console.log(myArray[k]);
+    k++
+}
+
+// do while
+let j= 11
+do{
+    console.log(j);
+    j++
+}
+while(j<10)
