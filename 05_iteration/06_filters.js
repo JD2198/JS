f5167934310cb115bcd7d3a535602d1a2becb64c
JD2198 <110241674+JD2198@users.noreply.github.com>
2024-01-06 05:47:12 +0000

    Filter

--- 05_iteration/06_filters.js
+++ 05_iteration/06_filters.js
@@ -22,12 +22,12 @@ const code = coding.forEach((item) => {
 const myNums = [1,2,3,4,5,6,7,8,9,10]
 
 // filter also takes callback as foreach but unlike forEach it will return value (if opened scope then write return, if () or in that line no need to write return)
-const val1 = myNums( (num) => num>4)
+
 const val= myNums.filter( (num) => {
     return num> 4
 })   
 
-console.log(val);
+// console.log(val);
 
 // how to use above one in for each
 
@@ -39,6 +39,24 @@ myNums.forEach( (num) => {
     }
 })
 
-console.log(newNums);
+// console.log(newNums);
 
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+  // array books mein filter laga rahe har ek obj mein as bk and condn lagayenge 
+let userbooks = books.filter( (bk) => bk.genre==='History')
+
+userbooks = books.filter( (bk) => bk.publish >= 1995 && bk.genre === 'History')
+
+console.log(userbooks);
  
